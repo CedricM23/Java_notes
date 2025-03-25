0bf45a28910120a02e8821c68252efc8449021ab
CedricM23 <Cedricpbc@gmail.com>
2025-03-25 19:23:52 -0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> names = new ArrayList<>();
+        List<LocalDate> dates = new ArrayList<LocalDate>();
+        List<Integer> amounts = new ArrayList<>();
+        List<String> test = new ArrayList<>(Arrays.asList("Luka", "Leia", "Han"));
+
+        names.add("Luke");
+        names.add("Leia");
+        names.add("Han");
+        names.add("Leia");
+
+
+
+    }
+}
